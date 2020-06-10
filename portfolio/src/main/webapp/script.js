4fb06feb14324ecfbe94714b85b08a912cfbf551
Keiffer Acoba <keiffer@google.com>
2020-06-10 15:19:51 -0400

    Add map code

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -52,3 +52,10 @@ function handleFetchErrors(response) {
   }
   return response;
 } 
+
+/** Creates a map and adds it to the page. */
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
