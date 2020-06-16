918b9e05f329d40fa56940b00b48870b36388aac
Keiffer Acoba <keiffer.acoba@gmail.com>
2020-06-16 19:01:37 -0400

    Implement getAttendeeAvailability

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,46 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public final class FindMeetingQuery {
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+    Collection<String> attendees = request.getAttendees();
+    ArrayList<Collection<TimeRange>> availabilities = new ArrayList<Collection<TimeRange>>();
+    availabilities.add(Arrays.asList(TimeRange.WHOLE_DAY));
+
+    for (String attendee : attendees) {
+      availabilities.add(getAttendeeAvailability(events, attendee));
+    }
+
     throw new UnsupportedOperationException("TODO: Implement this method.");
   }
+
+  /**
+   * Gets the available times in a day that the given attendee is available.
+   *
+   * @param events {@code Event} Collection for the day.
+   * @param attendee Person who we wish to find the available times.
+   * @return {@code TimeRange} Collection where the attendee is available.
+   */
+  private Collection<TimeRange> getAttendeeAvailability(Collection<Event> events, String attendee) {
+    Collection<TimeRange> availableTimes = new ArrayList<TimeRange>();
+
+    int availableStart = TimeRange.START_OF_DAY;
+    for (Event event : events) {
+      if (event.getAttendees().contains(attendee)) {
+        int eventStart = event.getWhen().start();
+        int eventEnd = event.getWhen().end();
+
+        availableTimes.add(TimeRange.fromStartEnd(availableStart, eventStart, false));
+        availableStart = eventEnd;
+      }
+    }
+    availableTimes.add(TimeRange.fromStartEnd(availableStart, TimeRange.END_OF_DAY, true));
+
+    return availableTimes;
+  }
+
 }
